0fd229d650769038371c0f8ea87a2b0fd611909c
KayoCosta <kaayo.costa@hotmail.com>
2022-09-20 14:57:53 -0300

    Difference

---
+++ Diferenca/Diferenca/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace SomaSimples
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+
+            int A, B, C, D;
+            A = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            B = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            C = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            D = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            int diferenca = ((A * B) - (C * D));
+
+            Console.WriteLine ("DIFERENCA = " + diferenca);
+
+        }
+
+    }
+
+}
