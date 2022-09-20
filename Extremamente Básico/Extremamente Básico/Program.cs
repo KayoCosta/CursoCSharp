fa6315b2f522f156825a21291bfcdc0daf57240a
KayoCosta <kaayo.costa@hotmail.com>
2022-09-20 14:39:16 -0300

    First Exercise

---
+++ Extremamente Básico/Extremamente Básico/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExtremamenteBasico
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+
+            int A, B, X;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            X = A + B;
+
+            Console.WriteLine("X = " + X);
+
+            
+        }
+
+    }
+    
+}
+    
