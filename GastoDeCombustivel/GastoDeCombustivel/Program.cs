8d21513fe45447b6014cdc90288b34d896ce3d41
KayoCosta <kaayo.costa@hotmail.com>
2022-09-20 15:01:53 -0300

    Fuel Consumption

---
+++ GastoDeCombustivel/GastoDeCombustivel/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace SomaSimples
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int tempo, velocidadeMedia;
+            tempo = int.Parse(Console.ReadLine());
+            velocidadeMedia = int.Parse(Console.ReadLine());
+
+            double consumo = (tempo * velocidadeMedia / 12.0);
+            Console.WriteLine(consumo.ToString("F3", CultureInfo.InvariantCulture));
+
+
+        }
+
+    }
+
+}
