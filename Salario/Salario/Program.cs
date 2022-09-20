ddf351da70f0f91200067efa97aa310a9ac49333
KayoCosta <kaayo.costa@hotmail.com>
2022-09-20 14:58:20 -0300

    Salary

---
+++ Salario/Salario/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace SomaSimples
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int cod1, cod2, qte1, qte2;
+            double preco1, preco2, total;
+
+            string[] valores = Console.ReadLine().Split(' ');
+            cod1 = int.Parse(valores[0]);
+            qte1 = int.Parse(valores[1]);
+            preco1 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            valores = Console.ReadLine().Split(' ');
+            cod2 = int.Parse(valores[0]);
+            qte2 = int.Parse(valores[1]);
+            preco2 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            total = qte1 * preco1 + qte2 * preco2;    
+
+
+            Console.WriteLine("VALOR A PAGAR: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+    }
+
+}
