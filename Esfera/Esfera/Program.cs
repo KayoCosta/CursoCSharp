019c139b031e4b0fd9442e1495469ca1ef52f858
KayoCosta <kaayo.costa@hotmail.com>
2022-09-20 15:03:41 -0300

    Sphere

---
+++ Esfera/Esfera/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace PrimeiroProjeto
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double volume = (4 / 3.0) * 3.14159 * Math.Pow(r, 3.0);
+
+            Console.WriteLine("VOLUME = " + volume.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
