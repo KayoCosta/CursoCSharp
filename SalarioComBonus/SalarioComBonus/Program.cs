77dbac9128e860fc403b2e48ee7e10868d955dcf
KayoCosta <kaayo.costa@hotmail.com>
2022-09-20 15:03:00 -0300

    Salary plus bonus

---
+++ SalarioComBonus/SalarioComBonus/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace PrimeiroProjeto
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = (Console.ReadLine());
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double vendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double total = salario + (0.15 * vendas);
+
+            Console.WriteLine("TOTAL = R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+        }
+    }
+}
