dc9899c272ed75ed273b1a363a9c92667caed5dd
KayoCosta <kaayo.costa@hotmail.com>
2022-09-20 14:57:08 -0300

    Average - 2

---
+++ Media2/Media2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace SomaSimples
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+
+            double A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double MEDIA = ((A * 2.0) + (B * 3.0) + (C * 5.0)) / 10;
+
+            Console.WriteLine("MEDIA = " + MEDIA.ToString("F1", CultureInfo.InvariantCulture));
+
+
+        }
+
+    }
+
+}
