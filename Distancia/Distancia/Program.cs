4bd88989f51ddd53fafdf7cb5770208c9438d050
KayoCosta <kaayo.costa@hotmail.com>
2022-09-20 14:58:56 -0300

    Distance

---
+++ Distancia/Distancia/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace SomaSimples
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int distancia = int.Parse(Console.ReadLine());
+            int tempo = distancia * 2;
+
+            Console.WriteLine(tempo + " minutos");
+
+        }
+
+    }
+
+}
