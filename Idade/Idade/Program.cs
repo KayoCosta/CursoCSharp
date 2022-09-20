17a49a2b5c85971fdb467dcc098fcfb70b0759fd
KayoCosta <kaayo.costa@hotmail.com>
2022-09-20 15:30:51 -0300

    Age

---
+++ Idade/Idade/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Idade
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N, anos, restoanos, meses, dias;
+
+            N = int.Parse(Console.ReadLine());
+            anos = N / 365;
+            restoanos = N % 365;
+            meses = restoanos / 30;
+            dias =  restoanos % 30;                      
+            
+            Console.WriteLine(anos + " ano(s)");
+            Console.WriteLine(meses + " mes(es)");
+            Console.WriteLine(dias + " dia(s)");
+            
+        }
+    }
+}
