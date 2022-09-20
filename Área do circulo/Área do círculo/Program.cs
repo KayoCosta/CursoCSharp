499d1eb9b2741aa0acffdd888039b38a1464586c
KayoCosta <kaayo.costa@hotmail.com>
2022-09-20 14:42:57 -0300

    Circle Area

---
+++ Área do circulo/Área do círculo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExtremamenteBasico{
+    class Program {
+        static void Main(String[] args)
+        {
+
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double pi = 3.14159;
+            double A = pi * raio * raio;
+
+            Console.WriteLine("A=" + A.ToString("F4",CultureInfo.InvariantCulture));
+
+        }
+
+    }
+
+}
+
