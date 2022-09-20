d5cd95758b2b6a033b12ce95ace9abcb2a59b164
KayoCosta <kaayo.costa@hotmail.com>
2022-09-20 14:58:39 -0300

    Consumption

---
+++ Consumo/Consumo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace SomaSimples
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int distancia = int.Parse(Console.ReadLine());
+            double combustivel = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double consumo = distancia / combustivel;
+
+            Console.WriteLine(consumo.ToString("F3", CultureInfo.InvariantCulture) + " km/l");
+
+          
+        }
+
+    }
+
+}
