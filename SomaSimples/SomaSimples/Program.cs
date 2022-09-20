ebb3261a4d79dff6fe7bab06c6f3e878e0d9a86f
KayoCosta <kaayo.costa@hotmail.com>
2022-09-20 14:48:40 -0300

    Simple Sum

---
+++ SomaSimples/SomaSimples/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace SomaSimples
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int SOMA = A + B;
+
+            Console.WriteLine("SOMA = " + SOMA);
+
+
+
+        }
+
+    }
+
+}
