9e890f6710985f6f61468bee54ebecba54770c3c
KayoCosta <kaayo.costa@hotmail.com>
2022-09-20 14:52:48 -0300

    Simple Product

---
+++ ProdutoSimples/ProdutoSimples/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace SomaSimples
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int PROD = A * B;
+
+            Console.WriteLine("PROD = " + PROD);
+
+
+
+        }
+
+    }
+
+}
