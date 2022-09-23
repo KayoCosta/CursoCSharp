3d36058361aa5e799c7e2c75839580d18fb5997a
KayoCosta <kaayo.costa@hotmail.com>
2022-09-22 23:03:06 -0300

    Phone Plan

---
+++ Aula 36 - PlanoTelefonico/PlanoTelefonico/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace PlanoTelefonico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutos = int.Parse(Console.ReadLine());
+            double x = 50.0;
+
+            if (minutos < 100)
+            {
+                Console.WriteLine("Valor a pagar: R$ " + x);
+            }
+            else
+            {
+                double y = ((minutos - 100) * 2);
+                double z = (x + y);
+
+                Console.WriteLine("Valor a pagar: R$ " + z.ToString("F2",CultureInfo.InvariantCulture));
+
+            }
+            
+        }
+    }
+}
