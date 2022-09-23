b9de5389bbf83e0316064d6ec131d8882a79c6a8
KayoCosta <kaayo.costa@hotmail.com>
2022-09-22 22:44:38 -0300

    Student's Note

---
+++ Aula 33 - Nota Aluno/NotaAluno/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace NotaAluno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, notafinal;
+
+            nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); ;
+
+            notafinal = nota1 + nota2;
+
+            if (notafinal < 60.00)
+            {
+                Console.WriteLine("NOTA FINAL = " + notafinal.ToString("F1", CultureInfo.InvariantCulture));
+                Console.WriteLine("REPROVADO");
+            }
+            
+
+
+
+        }
+    }
+}
