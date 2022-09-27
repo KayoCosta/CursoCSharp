e4779ef917ec67385665835bbc059aa8c98be8ed
KayoCosta <kaayo.costa@hotmail.com>
2022-09-27 18:25:14 -0300

    Note Validation

---
+++ ValidacaoDeNota/ValidacaoDeNota/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace ValidacaoDeNota
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, media;            
+            
+            nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            while (nota1 < 0 || nota1 > 10)
+            {                
+                Console.WriteLine("nota invalida");
+                nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);                    
+                
+            }
+
+            nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            while (nota2 < 0 || nota2 > 10)
+            {
+                Console.WriteLine("nota invalida");
+                nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            }
+
+            media = (nota1 + nota2) / 2;
+            Console.WriteLine("media = " + media.ToString("F2", CultureInfo.InvariantCulture));            
+        }
+    }
+}
