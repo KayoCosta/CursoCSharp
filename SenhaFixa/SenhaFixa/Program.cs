7a983bbb72e1e5dbdf9f88f8adc813d3a0c8e373
KayoCosta <kaayo.costa@hotmail.com>
2022-09-27 18:04:32 -0300

    Password

---
+++ SenhaFixa/SenhaFixa/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha;
+
+            senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {                
+                Console.WriteLine("Senha Invalida");
+                senha = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso Permitido");
+        }
+    }
+}
