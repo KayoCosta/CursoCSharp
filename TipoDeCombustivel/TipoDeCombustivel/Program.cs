91f70ca3ae7fd20c5ba9b75f37810c3a4277075f
KayoCosta <kaayo.costa@hotmail.com>
2022-09-27 18:41:51 -0300

    Fuel Type

---
+++ TipoDeCombustivel/TipoDeCombustivel/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace TipoDeCombustivel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            
+            int alcool = 0;
+            int gasolina = 0;
+            int diesel = 0;
+
+            int combustivel = int.Parse(Console.ReadLine());
+
+            while (combustivel != 4)
+            {                
+                if (combustivel == 1)
+                {
+                    alcool = alcool + 1;                    
+                }
+                else if (combustivel == 2)
+                {
+                    gasolina = gasolina + 1;                    
+                }
+                else if (combustivel == 3)
+                {
+                    diesel = diesel + 1;                    
+                }
+
+                combustivel = int.Parse(Console.ReadLine());                                
+            }
+
+            Console.WriteLine("MUITO OBRIGADO");
+            Console.WriteLine("Alcool: " + alcool);
+            Console.WriteLine("Gasolina: " + gasolina);
+            Console.WriteLine("Diesel: " + diesel);
+        }
+    }
+}
