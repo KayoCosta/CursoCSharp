9f8f03b66b62cd0d6821e979d402be7850e8aed5
KayoCosta <kaayo.costa@hotmail.com>
2022-09-27 21:00:02 -0300

    Test: For

---
+++ TesteFor/TesteFor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace TesteFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, x, soma;
+            
+            N = int.Parse(Console.ReadLine());
+
+            soma = 0;
+
+            //i++ vale a mesma coisa que i = i + 1
+            for (int i=1; i<=N; i++)
+            {
+                x = int.Parse(Console.ReadLine());
+                soma = soma + x;
+            }
+
+            Console.WriteLine(soma);
+        }
+    }
+}
