c106e44e8d9bdd26046432397209a4b29a1c2660
KayoCosta <kaayo.costa@hotmail.com>
2022-09-29 16:05:21 -0300

    Vector Test

---
+++ TesteVetores/TesteVetores/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace TesteVetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            double[] vet;
+
+            N = int.Parse(Console.ReadLine());
+            vet = new double[N];
+
+
+            for (int i = 0; i < N; i++)
+            {
+                vet[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(vet[i].ToString("F1",CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
